f1745584cd171e8eae8f980626da80382c651a9c
Mazen Ahmed <105662003+Mazen17Ahmed4@users.noreply.github.com>
2023-09-05 17:46:08 +0300

    Add files via upload

---
+++ Final Candy Crush Project/Candy_Crash_Game/CandyCrushGame/src/candycrushgame/Level_Scores.java
@@ -0,0 +1,186 @@
+package candycrushgame;
+
+import java.io.*;
+import java.util.Scanner;
+
+public class Level_Scores {
+
+    public static int countOfLevel;
+    public static int lastLevel;
+    public boolean checkLevel = false;
+    private int score;
+    private int move = 15;
+    private int level;
+    private String projectDir = System.getProperty("user.dir");
+
+    public int level() {
+        /*--------------------------------------------------------------------------------------------*/
+        /*----------------------- Set level according to the score --------------------------------- */
+        /*------------------------------------------------------------------------------------------*/
+        int currentLevel = level;
+        if (score <= 100) {
+            level = 1;
+
+        } else if (score > 100 && score <= 250) {
+            level = 2;
+        } else if (score > 250 && score <= 450) {
+            level = 3;
+        } else if (score > 450 && score <= 700) {
+            level = 4;
+        } else if (score > 700 && score <= 1000) {
+            level = 5;
+        } else if (score > 1000 && score <= 1300) {
+            level = 6;
+        } else if (score > 1300 && score <= 1650) {
+            level = 7;
+        } else if (score > 1650 && score <= 2050) {
+            level = 8;
+        } else if (score > 2050 && score <= 2500) {
+            level = 9;
+        } else if (score > 2500 && score <= 3000) {
+            level = 10;
+        }
+        System.out.println(level);
+        System.out.println(currentLevel);
+        if (getmoves() >= 0 && level > currentLevel && currentLevel > 0) {
+            PopoutWindow popoutWindow = new PopoutWindow();
+            popoutWindow.lbl.setText("Congratulation");
+            move = 15;
+            secondWindow.movesLbl1.setText("" + getmoves());
+            popoutWindow.show();
+            saveLevel();
+        } else if (getmoves() == 0 && level == currentLevel && currentLevel > 0) {
+            PopoutWindow popoutWindow = new PopoutWindow();
+            popoutWindow.lbl.setText("Game Over");
+            move = 15;
+            score = 0;
+            getScore();
+            secondWindow.movesLbl1.setText("" + getmoves());
+            popoutWindow.show();
+        }
+        return level;
+    }
+
+    public String getScore() {
+        /*--------------------------------------------------------------------------------------------*/
+        /*----------------------- Set record score according to the level--------------------------- */
+        /*------------------------------------------------------------------------------------------*/
+        String maxScore = "";
+        switch (level) {
+            case 1 ->
+                maxScore = "/100";
+            case 2 ->
+                maxScore = "/250";
+            case 3 ->
+                maxScore = "/450";
+            case 4 ->
+                maxScore = "/700";
+            case 5 ->
+                maxScore = "/1000";
+            case 6 ->
+                maxScore = "/1300";
+            case 7 ->
+                maxScore = "/1650";
+            case 8 ->
+                maxScore = "/2050";
+            case 9 ->
+                maxScore = "/2500";
+            case 10 ->
+                maxScore = "/3000";
+            default -> {
+            }
+        }
+        return ("" + score + maxScore);
+    }
+
+    public void saveLevel() {
+        /*-------------------------------------------------------------------------------------------------*/
+        /*--------------------- save level in txt file for the next game ---------------------------------*/
+        /*-----------------------------------------------------------------------------------------------*/
+        try {
+
+            File file = new File(projectDir + "\\src\\level_score.txt");
+            FileWriter fw = new FileWriter(file);
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write("Level: " + level);
+            bw.newLine();
+            bw.write("Score: " + getScore());
+            bw.close();
+            System.out.println("Level and score saved to file.");
+            fw.close();
+        } catch (IOException e) {
+            System.out.println("Error occurred while saving level and score to file.");
+            e.printStackTrace();
+        }
+    }
+
+    public int loadLevel() {
+        /*-------------------------------------------------------------------------------------------------*/
+        /*----------------------------------- load level value -------------------------------------------*/
+        /*-----------------------------------------------------------------------------------------------*/
+        try {
+            File file = new File(projectDir + "\\src\\level_score.txt");
+
+            Scanner scanner = new Scanner(file);
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                if (line.startsWith("Level: ")) {
+                    level = Integer.parseInt(line.substring(7));
+                }
+                else if (line.startsWith("Score: ")) {
+                    score = Integer.parseInt(line.substring(7,line.indexOf("/")));
+            }
+            scanner.close();
+            System.out.println("Level and score loaded from file.");
+        }
+    }catch (IOException e) {
+            System.out.println("Error occurred while loading level and score from file.");
+            e.printStackTrace();
+        }
+        return level;
+    }
+
+    public void moves() {
+        move--;
+
+    }
+
+    /*-------------------------------------------------------------------------------------------------*/
+    /*----------------------------------- return values and get values -------------------------------*/
+    /*-----------------------------------------------------------------------------------------------*/
+    public void score1() {
+        score += 10;
+    }
+
+    public void score2() {
+        score += 15;
+    }
+
+    public void setMove(int move) {
+        this.move = move;
+    }
+
+    public int getmoves() {
+        return move;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public int getcountOfLevel() {
+        return countOfLevel;
+    }
+
+    public int getLastLevel() {
+
+        return lastLevel;
+
+    }
+
+    public void setScore(int score) {
+
+        this.score = score;
+    }
+
+}
---
+++ Final Candy Crush Project/Candy_Crash_Game/CandyCrushGame/src/candycrushgame/PopoutWindow.java
@@ -0,0 +1,132 @@
+package candycrushgame;
+
+import java.util.Optional;
+import javafx.application.Platform;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundSize;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.stage.Stage;
+
+class btnpop extends Button {
+
+    public btnpop(String s) {
+
+        setMinSize(270, 40);
+        setText(s);
+        setOpacity(0.8);
+
+    }
+}
+
+public class PopoutWindow extends Stage {
+
+    Level_Scores levelScores = new Level_Scores();
+    public Label lbl;
+
+    public PopoutWindow() {
+        // Create the layout for the popout window
+        HBox layout1 = new HBox(10);
+        HBox layout2 = new HBox(10);
+        HBox layout3 = new HBox(10);
+        VBox root = new VBox();
+        // padding
+        layout2.setPadding(new Insets(7));
+        // center
+        layout1.setAlignment(Pos.BASELINE_CENTER);
+        layout2.setAlignment(Pos.BASELINE_CENTER);
+        layout3.setAlignment(Pos.BASELINE_CENTER);
+        lbl = new Label();
+        // if ((levelScores.getmoves() > 0 && levelScores.getcountOfLevel() >
+        // levelScores.getLastLevel())
+        // || (levelScores.getmoves() == 0 && levelScores.getcountOfLevel() ==
+        // levelScores.getLastLevel() + 1)) {
+        // lbl.setText("Congratulation");
+        // levelScores.saveLevelScore();
+        // } else {// if ( levelScores.getmoves() < 0 ){
+
+        // lbl.setText("GameOver");
+        // }
+        // create Label
+
+        // lbl size
+        lbl.setBackground(new Background(new BackgroundFill(Color.ALICEBLUE, CornerRadii.EMPTY, Insets.EMPTY)));
+        lbl.setFont(new Font("Cambria", 32));
+        lbl.setPadding(new Insets(7));
+        lbl.setTextFill(Color.PURPLE);
+        lbl.setOpacity(0.8);
+
+        // create buttons
+        btnpop newgamebtn = new btnpop("New Game");
+        newgamebtn.setOnAction(event -> {
+
+            levelScores.setScore(0);
+            // try {
+            //     secondWindow.scorelbl1.setText("" + levelScores.getScore());
+            // } catch (Exception e) {
+            //     // Handle the exception here
+            //     System.out.println("An error occurred while setting the score label: " + e.getMessage());
+            // }
+            
+            this.close();
+        });
+        btnpop closeButton = new btnpop("Close");
+        // set close button on action
+        closeButton.setOnAction(e -> {
+
+            Alert A = new Alert(Alert.AlertType.CONFIRMATION);
+            A.setTitle("Confirm Exit");
+            A.setContentText("Are you sure , you want to exit ?");
+            // A.setContentText("Pay attention please, All unsaved changes will be lost.");
+            Optional<ButtonType> result = A.showAndWait();
+            if (result.isPresent() && result.get() == ButtonType.OK) {
+                Platform.exit();
+            }
+        });
+
+        // add items
+        layout1.getChildren().add(lbl);
+        layout2.getChildren().add(newgamebtn);
+        layout3.getChildren().add(closeButton);
+
+        // create an Image object from the file path
+        String projectDir = System.getProperty("user.dir"); // get current project direction
+        String img_dir = "\\src\\image\\popout.jpg"; // image dir
+        Image image = new Image("file:" + projectDir + img_dir);
+
+        // create a BackgroundImage object with the Image
+        BackgroundImage backgroundImage = new BackgroundImage(image, null, null, null,
+                new BackgroundSize(BackgroundSize.AUTO, BackgroundSize.AUTO, false, false, true, false));
+
+        // create a Background object with the BackgroundImage
+        Background background = new Background(backgroundImage);
+
+        // set the background to the root StackPane object
+        root.setBackground(background);
+
+        // Create the scene for the popout window
+        root.getChildren().addAll(layout1, layout2, layout3);
+        root.setAlignment(Pos.BOTTOM_CENTER);
+        root.setPadding(new Insets(40));
+        Scene scene = new Scene(root, 300, 350);
+
+        // Set the scene for the popout window
+        this.setScene(scene);
+        this.setTitle("Candy Crush");
+
+    }
+
+}
